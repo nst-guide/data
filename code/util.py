9a1f8d68c4861045fe428b3f98a5bcd7325270fd
Kyle Barron <kylebarron2@gmail.com>
2019-10-14 17:46:24 -0400

    Add function for OSM poly -> geojson

--- code/util.py
+++ code/util.py
@@ -2,6 +2,7 @@ from functools import partial
 
 import pyproj
 from shapely.ops import transform
+import geojson
 
 WGS = 'epsg:4326'
 WEB_MERCATOR = 'epsg:3857'
@@ -22,3 +23,27 @@ def wgs_to_web_mercator(obj):
 
 def web_mercator_to_wgs(obj):
     return reproject(obj, WEB_MERCATOR, WGS)
+
+def osm_poly_to_geojson(lines):
+    lines = [line.rstrip() for line in lines]
+    name = lines[0]
+
+    section_starts = [ind for ind, line in enumerate(lines) if line[:3] not in ['   ', 'END']]
+    section_ends = [ind for ind, line in enumerate(lines) if line[:3] == 'END']
+
+    features = []
+    for start, end in zip(section_starts, section_ends):
+        part = lines[start:end]
+        section_name = part[0]
+        coords = [line.split() for line in part[1:]]
+
+        # Convert str to float
+        # the float() function handles the scientific notation
+        coords = [(float(coord[0]), float(coord[1])) for coord in coords]
+
+        polygon = geojson.Polygon(coordinates=coords)
+        feature = geojson.Feature(geometry=polygon, properties={'name': section_name})
+        features.append(feature)
+
+    feature_collection = geojson.FeatureCollection(features)
+    return feature_collection
