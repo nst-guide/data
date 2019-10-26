edc63de5db1c8b93664c038528e0496441ff3f34
Kyle Barron <kylebarron2@gmail.com>
2019-10-26 22:05:23 +0100

    Add Transit data from transit.land

--- code/data.py
+++ code/data.py
@@ -1,5 +1,6 @@
 # General downloads
 
+import json
 import os
 import re
 from io import BytesIO
@@ -19,13 +20,14 @@ import requests
 from dotenv import load_dotenv
 from fiona.io import ZipMemoryFile
 from geopandas.tools import sjoin
+from haversine import haversine
 from lxml import etree as ET
 from shapely.geometry import LineString, MultiLineString, box, mapping, shape
 from shapely.ops import linemerge
 
 import geom
-from grid import TenthDegree
 import util
+from grid import TenthDegree
 
 
 def in_ipython():
@@ -87,8 +89,6 @@ class OpenStreetMap(DataSource):
 
     def create_extracts(self):
         """Creates .o5m file for buffer area around trail
-
-        TODO: include town geometries
         """
         states = ['california', 'oregon', 'washington']
         self._download_states(states, overwrite=False)
@@ -580,3 +580,164 @@ class LightningCounts(DataSource):
 
         merged = df.merge(center_df, how='inner')
         return merged
+
+
+class Transit(DataSource):
+    def __init__(self):
+        super(Transit, self).__init__()
+        self.trail = USFS().trail().iloc[0].geometry
+
+        self.routes = {}
+        self.nearby_stops = {}
+        self.all_stops = {}
+        self.operators = {}
+
+    def download(self):
+        """Create trail-relevant transit dataset from transit.land database
+        """
+
+        # Find operators that intersect trail
+        operators_near_trail = self.get_operators_near_trail()
+
+        # For each operator, see if there are actually transit stops within a
+        # walkable distance from the trail (currently set to 1000m)
+        for operator in operators_near_trail:
+            # First, see if this operator actually has stops near the trail
+            stops = self.get_stops_near_trail(operator['onestop_id'],
+                                              distance=1000)
+            if len(stops) == 0:
+                continue
+
+            for stop in stops:
+                # Add stop to the self.nearby_stops dict
+                stop_id = stop['onestop_id']
+                self.nearby_stops[stop_id] = stop
+
+                # Get more info about each route that stops at stop
+                # Routes are added to self.routes
+                self.get_routes_for_stop(stop)
+
+        self.update_stop_information()
+        # self.get_schedules()
+
+        # Serialize everything to disk
+        save_dir = self.data_dir / 'pct' / 'line' / 'transit'
+        save_dir.mkdir(parents=True, exist_ok=True)
+
+        with open(save_dir / 'routes.json', 'w') as f:
+            json.dump(self.routes, f)
+        with open(save_dir / 'nearby_stops.json', 'w') as f:
+            json.dump(self.nearby_stops, f)
+        with open(save_dir / 'all_stops.json', 'w') as f:
+            json.dump(self.all_stops, f)
+
+    def get_operators_near_trail(self):
+        """Find transit operators with service area crossing the trail
+
+        Using the transit.land API, you can find all transit operators within a
+        bounding box. Since the bbox of the PCT is quite large, I then check the
+        service area polygon of each potential transit operator to see if it
+        intersects the trail.
+        """
+        trail_bbox = self.trail.bounds
+        trail_bbox = [str(x) for x in trail_bbox]
+
+        url = 'https://transit.land/api/v1/operators'
+        params = {'bbox': ','.join(trail_bbox), 'per_page': 10000}
+        r = requests.get(url, params=params)
+        d = r.json()
+
+        operators_on_trail = []
+        for operator in d['operators']:
+            # Check if the service area of the operator intersects trail
+            operator_polygon = shape(operator['geometry'])
+            intersects_trail = self.trail.intersects(operator_polygon)
+            if intersects_trail:
+                operators_on_trail.append(operator)
+
+        return operators_on_trail
+
+    def get_stops_near_trail(self, operator_id, distance=1000):
+        """Find all stops in Transitland database that are near trail
+
+        Args:
+            operator_id: onestop operator id
+            distance: distance to trail in meters
+        """
+        url = 'https://transit.land/api/v1/stops'
+        params = {'served_by': operator_id, 'per_page': 10000}
+        r = requests.get(url, params=params)
+        d = r.json()
+
+        stops_near_trail = []
+        for stop in d['stops']:
+            point = shape(stop['geometry'])
+            nearest_trail_point = self.trail.interpolate(
+                self.trail.project(point))
+
+            dist = haversine(*point.coords,
+                             *nearest_trail_point.coords,
+                             unit='m')
+            if dist < distance:
+                stop['distance_to_trail'] = dist
+                stops_near_trail.append(stop)
+
+        return stops_near_trail
+
+    def get_routes_for_stop(self, stop):
+        for route_serving_stop in stop['routes_serving_stop']:
+            route_onestop_id = route_serving_stop['route_onestop_id']
+            url = f'https://transit.land/api/v1/onestop_id/{route_onestop_id}'
+            if self.routes.get(route_onestop_id) is None:
+                r = requests.get(url)
+                d = r.json()
+                self.routes[route_onestop_id] = d
+                route_stops = d['stops_served_by_route']
+
+                # Initialize keys in self.stops to fill in later with full stop
+                # information
+                for route_stop in route_stops:
+                    route_stop_id = route_stop['stop_onestop_id']
+                    self.all_stops[route_stop_id] = self.all_stops.get(
+                        route_stop_id)
+
+    def update_stop_information(self):
+        """Update stop information from Transit land
+
+        Stops are initialized by key in `get_routes_for_stop` but have no data.
+        Fill in that data here.
+        """
+        for stop_id, value in self.all_stops.items():
+            if value is not None:
+                continue
+
+            url = f'https://transit.land/api/v1/onestop_id/{stop_id}'
+            r = requests.get(url)
+            self.all_stops[stop_id] = r.json()
+
+    def get_schedules(self):
+        """Get schedules to add to route and stop data
+
+        TODO figure out the best way to collect and store this
+        """
+        url = 'https://transit.land/api/v1/schedule_stop_pairs'
+        for route_id in self.routes.keys():
+            params = {'route_onestop_id': route_id, 'per_page': 10000}
+            r = requests.get(url, params=params)
+            d = r.json()
+
+    def get_geojson_for_routes(self):
+        """Create FeatureCollection from self.routes for inspection
+        """
+        features = []
+        for route_id, route in self.routes.items():
+            properties = {
+                'onestop_id': route['onestop_id'],
+                'name': route['name'],
+                'vehicle_type': route['vehicle_type'],
+                'operated_by_name': route['operated_by_name'],
+            }
+            feature = geojson.Feature(geometry=route['geometry'],
+                                      properties=properties)
+            features.append(feature)
+        geojson.FeatureCollection(features)
