6f5a5a3211f41680711bf44e603bbaa26da6b4c3
Kyle Barron <kylebarron2@gmail.com>
2019-10-26 23:02:23 +0100

    Add code to load gps track files

--- code/data.py
+++ code/data.py
@@ -416,6 +416,61 @@ class USFS(DataSource):
                        driver='GeoJSON')
 
 
+class GPSTracks(DataSource):
+    def __init__(self):
+        super(GPSTracks, self).__init__()
+
+    def convert_fit_to_geojson(self):
+        """
+        The raw files of these GPS tracks are stored in the Git repository, but
+        they still need to be converted into a helpful format.
+
+        There doesn't appear to be a great Python package to work with .fit
+        files, so I'm using [GPSBabel][gpsbabel] to do the conversion.
+
+        [gpsbabel]: https://www.gpsbabel.org
+        """
+
+        gpsbabel_path = '/Applications/GPSBabelFE.app/Contents/MacOS/gpsbabel'
+        raw_dir = self.data_dir / 'raw' / 'tracks'
+
+        for fit_file in raw_dir.glob('*.fit'):
+            geojson_path = fit_file.parents[0] / (fit_file.stem + '.geojson')
+            cmd = [
+                gpsbabel_path, '-i', 'garmin_fit', '-f',
+                str(fit_file), '-o', 'geojson', '-F',
+                str(geojson_path)
+            ]
+            run(cmd, check=True)
+
+            gpx_path = fit_file.parents[0] / (fit_file.stem + '.gpx')
+            cmd = [
+                gpsbabel_path, '-i', 'garmin_fit', '-f',
+                str(fit_file), '-o', 'gpx', '-F',
+                str(gpx_path)
+            ]
+            run(cmd, check=True)
+
+        # Now join all of these together
+        features = []
+        for geojson_file in raw_dir.glob('*.geojson'):
+            with open(geojson_file) as f:
+                d = geojson.load(f)
+
+            features.extend(d['features'])
+
+        fc = geojson.FeatureCollection(features)
+        save_dir = self.data_dir / 'pct' / 'line' / 'gps_track'
+        save_dir.mkdir(exist_ok=True, parents=True)
+        with open(save_dir / 'gps_track.geojson', 'w') as f:
+            geojson.dump(fc, f)
+
+    def trail(self):
+        gdf = gpd.read_file(self.data_dir / 'pct' / 'line' / 'gps_track' /
+                            'gps_track.geojson')
+        return gdf
+
+
 class PolygonSource(DataSource):
     def __init__(self):
         super(PolygonSource, self).__init__()
