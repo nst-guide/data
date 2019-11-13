0669909d22f1b1d88d1285c62460832c9f6ca6b5
Kyle Barron <kylebarron2@gmail.com>
2019-11-13 13:11:40 -0700

    Save recreation.gov and calfire progress

--- code/data.py
+++ code/data.py
@@ -1856,3 +1856,65 @@ class GeoMAC(DataSource):
             d[k.lower().replace(' ', '_')] = v
 
         return d
+
+
+class CalFire(DataSource):
+    def __init__(self):
+        super(CalFire, self).__init__()
+
+        self.raw_dir = self.data_dir / 'raw' / 'calfire'
+        self.raw_dir.mkdir(parents=True, exist_ok=True)
+
+    def download(self, overwrite=False):
+        url = 'https://frap.fire.ca.gov/media/2525/fire18_1.zip'
+        local_path = self.raw_dir / Path(url).name
+        if overwrite or (not local_path.exists()):
+            urlretrieve(url, local_path)
+
+    def perimeters(self):
+        local_path = self.raw_dir / 'fire18_1.zip'
+        perimeters = gpd.read_file(f'zip://{str(local_path)}!fire18_1.gdb')
+        import geopandas as gpd
+        perimeters = gpd.read_file(
+            'zip:///Users/kyle/github/mapping/nst-guide/create-database/data/raw/calfire/fire18_1.zip!fire18_1.gdb'
+        )
+        dict(perimeters['YEAR_'].value_counts())
+        perimeters
+        Visualize(perimeters)
+
+
+class RecreationGov(DataSource):
+    def __init__(self):
+        super(RecreationGov, self).__init__()
+
+        self.raw_dir = self.data_dir / 'raw' / 'recreationgov'
+        self.raw_dir.mkdir(parents=True, exist_ok=True)
+
+        load_dotenv()
+        self.api_key = os.getenv('RIDB_API_KEY')
+        assert self.api_key is not None, 'Missing Recreation.gov API Key'
+
+    def download(self, overwrite=False):
+        # To download all the RIDB recreation area, facility, and site level
+        # data in CSV or JSON format, please select the link below. Updated
+        # Daily.
+        url = 'https://ridb.recreation.gov/downloads/RIDBFullExport_V1_CSV.zip'
+        local_path = self.raw_dir / Path(url).name
+        if overwrite or (not local_path.exists()):
+            urlretrieve(url, local_path)
+
+    def get_campsites_near_trail(self, trail):
+        section_name, trail = next(Halfmile().trail_iter())
+        trail_buf = geom.buffer(trail, distance=2, unit='mile')
+        trail_buf = gpd.GeoDataFrame(geometry=trail_buf)
+        buf = geom.buffer(trail, distance=2, unit='mile').unary_union
+
+        local_path = self.raw_dir / 'RIDBFullExport_V1_CSV.zip'
+        z = ZipFile(local_path)
+        z.namelist()
+        df = pd.read_csv(BytesIO(z.read('Facilities_API_v1.csv')))
+        gdf = gpd.GeoDataFrame(
+            df,
+            geometry=df.apply(lambda row: Point(row['FacilityLongitude'], row[
+                'FacilityLatitude']),
+                              axis=1))
