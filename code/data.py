1e17d36cdca19cfeec61b79c735bbe24ac2be9b5
Kyle Barron <barronk@mit.edu>
2019-11-24 01:28:48 -0700

    Add wrapper for Wikipedia API

--- code/data.py
+++ code/data.py
@@ -22,6 +22,7 @@ import osmnx as ox
 import pandas as pd
 import rasterio
 import requests
+import wikipedia
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from fastkml import kml
@@ -2186,3 +2187,127 @@ class RecreationGov(DataSource):
                 lambda row: Point(
                     row['FacilityLongitude'], row['FacilityLatitude']),
                 axis=1))
+
+
+class Wikipedia(DataSource):
+    """
+    Wrapper to access the Wikipedia API
+    """
+    def __init__(self):
+        super(Wikipedia, self).__init__()
+        self.image_dir = self.data_dir / 'raw' / 'wikipedia' / 'images'
+
+    def find_page_by_name(self, name: str):
+        """Find a single Wikipedia page given a name
+
+        Search for the top 5 results. If there's a result with the exact same
+        normalized name, choose that article. Otherwise, choose the first
+        result.
+
+        Note that even when the name is not exactly the same, the first result
+        usually makes sense. For example, the first result for "Mount Rainier
+        Wilderness" is "Mount Rainier National Park". And it would be ok to show
+        the wikipedia page for the National Park when a user clicks on the
+        wilderness polygon.
+        """
+        res = wikipedia.search(name, results=5)
+        exact_match = [
+            ind for ind, s in enumerate(res)
+            if normalize_string(s) == normalize_string(name)
+        ]
+        choice = None
+        if exact_match != []:
+            choice = exact_match[0]
+        else:
+            choice = 0
+
+        return wikipedia.page(res[choice])
+
+    def page(self, title):
+        """Simple page wrapper around wikipedia.page"""
+        return wikipedia.page(title)
+
+    def get_html_for_page(self, page: wikipedia.WikipediaPage):
+        """Construct HTML for page
+
+        TODO: Fix HTML links so that images and external links work correctly.
+        For example, current wiki links are `href="/wiki/Mount_Lago"`, and that
+        should be changed to `href="wikipedia.org/wiki/Mount_Lago"`. Note that
+        it would be cool if you were able to check if the page pointed to is
+        also in the Wikipedia extract, because then you could link to it
+        offline!?!?
+        """
+        html = page.html()
+
+        # Download images
+        image_paths = self._download_images(page.images)
+
+        # Fix links
+        html = self._fix_links(html, image_paths)
+
+        return html
+
+    def _fix_links(self, html):
+        """Fix links in Wikipedia page HTML
+        """
+        return html
+
+    def _download_images(self, images: List[str]):
+        """Download linked images from Wikipedia page
+
+        Args:
+            - images: list of URLs to Wikipedia page images
+        """
+        local_paths = []
+        for image_url in images:
+            parsed = urlparse(image_url)
+            local_path = str(self.image_dir) + parsed.path
+            local_paths.append(local_path)
+            if not local_path.exists():
+                urlretrieve(image_url, local_path)
+
+        return local_paths
+
+    def find_page_titles_around_point(
+            self, point: Point, title=None, results=None,
+            radius=10000) -> List[str]:
+        """Find pages around point using Wikipedia Geosearch
+
+        Args:
+            - point: geographic point to search around
+            - title: optional title of nearby page
+            - results: max number of results. Not sure if None is unlimited
+            - radius: search radius in meters. Between 10 and 10,000
+
+        Returns:
+            - Page _titles_ around point. Loading actual page metadata is
+              slower, so that can be done in a later function, in case I want to
+              get just titles to filter.
+        """
+        assert isinstance(point, Point), 'point must have Point geometry'
+        lon, lat = list(point.coords)[0]
+
+        res = wikipedia.geosearch(
+            latitude=lat,
+            longitude=lon,
+            title=title,
+            results=results,
+            radius=radius)
+        return res
+
+    def find_page_titles_for_polygon(self, polygon):
+        """Find pages within polygon using repeated Geosearch
+
+        The Geosearch API has no polygon support; only point. To get around
+        this, I first find a collection of circles that together tile the
+        polygon, then for each circle I call the geosearch API.
+        """
+        circles = geom.find_circles_that_tile_polygon(polygon)
+        titles = set()
+        for circle in circles:
+            point = circle.center
+            radius = circle.radius
+            res = self.find_page_titles_around_point(point, radius=radius)
+            titles.update(res)
+
+        return titles
