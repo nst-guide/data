fa4d04b9943319e54f77094a5ea3a81deb1a2055
Kyle Barron <barronk@mit.edu>
2019-11-29 18:28:52 -0700

    Update WildernessConnectScraper to use base Scraper module

--- code/data_source/wilderness_net.py
+++ code/data_source/wilderness_net.py
@@ -1,10 +1,4 @@
-from time import sleep
-
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.common.exceptions import ElementNotVisibleException
-
-from .base import PolygonSource
+from .base import PolygonSource, Scraper
 
 
 class WildernessBoundaries(PolygonSource):
@@ -22,37 +16,29 @@ class WildernessBoundaries(PolygonSource):
 
         regs = []
         for row in gdf.itertuples(index=False):
-            scraper.get(row.URL)
-            regs.append(scraper.regulations())
+            regulations = scraper.get_regulations(row.URL)
+            regs.append(regulations)
 
         return regs
 
 
-class WildernessConnectScraper:
+class WildernessConnectScraper(Scraper):
     """docstring for WildernessConnectScraper"""
     def __init__(self):
         super(WildernessConnectScraper, self).__init__()
-        self.driver = webdriver.Chrome()
 
-    url = 'https://wilderness.net/visit-wilderness/?ID=382#area-management'
+    def get_regulations(self, url):
+        """Get regulations for wilderness area from wilderness.net
 
-    def get(self, url):
-        # Go to wilderness page
-        self.driver.get(url)
-        self._wait_for_load(css_selector='#regulations')
-        soup = BeautifulSoup(self.driver.page_source, 'lxml')
-        self._get_regulations(soup)
+        Args:
+            - url: url to wilderness.net page on given Wilderness area. Should
+              be sourced from wilderness.net boundaries file. E.g.:
 
-    def _wait_for_load(self, css_selector):
-        try:
-            regs = self.driver.find_element_by_css_selector(css_selector)
-        except ElementNotVisibleException:
-            sleep(1)
-            self._wait_for_load()
-
-    def _get_regulations(self, soup):
-        regs = soup.select('#regulations')
-        assert len(regs) == 1, '#regulations gives >1 result'
-        regs = regs[0]
-
-        print(regs.text)
+              https://wilderness.net/visit-wilderness/?ID=382#area-management
+        """
+        self.get(url)
+        regulations_selector = '#regulations'
+        self.wait_for(regulations_selector)
+        soup = self.html()
+        regulations = soup.select(regulations_selector)
+        return regulations
