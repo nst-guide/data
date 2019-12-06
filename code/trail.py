57fcb8d4cf6b9b824b5691a369ec8b3f8e4117d7
Kyle Barron <barronk@mit.edu>
2019-12-06 00:22:36 -0700

    Remove old, unused code to create convex hull of PCT

--- code/trail.py
+++ code/trail.py
@@ -528,44 +528,6 @@ class TrailSection:
             full_data.append(section_line)
 
 
-class PacificCrestTrail(Trail):
-    """Construct PCT dataset
-    """
-    def __init__(self):
-        super(PacificCrestTrail, self).__init__()
-
-        self.route = self.construct_route()
-        self.intersect_hydrography(self.route)
-
-    def convex_hull(self, overwrite=False):
-        """Create localized convex hull with trail buffer and towns
-        """
-        # Check if already created
-        path = data.DataSource().data_dir
-        path /= 'pct' / 'polygon' / 'convex_hull_buffer2.geojson'
-        if path.exists():
-            return gpd.read_file(path)
-
-        # Load all towns
-        town_gdf = Towns().boundaries()
-        section_polygons = []
-        for section, hm_gdf in Halfmile().trail_iter():
-            # Get town boundaries in section
-            nearby_towns = town_gdf[town_gdf['section'] == section]
-
-            # Get buffer around trail
-            hm_buffer = gpd.GeoDataFrame(
-                geometry=buffer(hm_gdf, distance=2, unit='mile'))
-
-            # Combine trail buffer and towns
-            towns_and_trails = pd.concat([hm_buffer, nearby_towns], sort=False)
-            section_polygons.append(towns_and_trails.unary_union.convex_hull)
-
-        section_polygons = gpd.GeoDataFrame(geometry=section_polygons)
-        section_polygons.to_file(path, driver='GeoJSON')
-        return section_polygons
-
-
 def intersect_trail_with_polygons(
         trail: LineString, gdf: gpd.GeoDataFrame, key_col: str):
     """Intersect trail with polygons to produce overlapping line segments
