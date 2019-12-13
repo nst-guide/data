cfa4977bf71a7fce35428fb6762d1fb22da87bb4
Kyle Barron <barronk@mit.edu>
2019-12-13 16:39:32 -0700

    Update package_tiles cli api

--- code/main.py
+++ code/main.py
@@ -14,7 +14,6 @@ def main():
 @click.option(
     '-g',
     '--geometry',
-    multiple=True,
     required=True,
     type=click.Path(
         exists=True, file_okay=True, dir_okay=False, resolve_path=True),
@@ -24,10 +23,10 @@ def main():
 @click.option(
     '-b',
     '--buffer',
-    multiple=True,
     required=True,
+    type=str,
     help=
-    'Buffer distance (in miles) to use around provided geometries. The same number of options as geometry must be provided. If you want multiple buffer distances, pass as --buffer "2 5 10"'
+    'Buffer distance (in miles) to use around provided geometries. If you want multiple buffer distances, pass as --buffer "2 5 10"'
 )
 @click.option(
     '-d',
@@ -50,6 +49,22 @@ def main():
     'Paths to tile.json files for each directory. If not provided, assumes a tile JSON file is at directory/tile.json. Otherwise, the same number of options as directory must be provided.'
 )
 @click.option(
+    '-z',
+    '--min-zoom',
+    type=int,
+    required=False,
+    default=0,
+    multiple=True,
+    help='Min zoom for each tile source')
+@click.option(
+    '-Z',
+    '--max-zoom',
+    type=int,
+    required=False,
+    default=14,
+    multiple=True,
+    help='Max zoom for each tile source')
+@click.option(
     '-o',
     '--output',
     type=click.Path(exists=False, writable=True, resolve_path=True),
@@ -62,35 +77,42 @@ def main():
     help=
     'Whether to raise an error if a desired tile is not found in the directory.'
 )
-def package_tiles(geometry, buffer, directory, tile_json, output, raise_errors):
+def package_tiles(
+        geometry, buffer, directory, tile_json, min_zoom, max_zoom, output,
+        raise_errors):
     """Package tiles into zip based on distance from trail
 
     Example:
     python main.py package-tiles -g ../data/pct/polygon/bound/town/ca/acton.geojson -b "0 1 2" -d ~/Desktop -o out/
     """
-    # Make sure that buffer and geometry have same dimensions
-    msg = 'geometry and buffer must be provided the same number of times'
-    assert len(geometry) == len(buffer), msg
-
-    # Make sure that buffer and geometry have same dimensions
+    # Make sure that tile_json and directory have same dimensions
     if tile_json:
         msg = 'tile-json and directory must be provided the same number of times'
-        assert len(geometry) == len(buffer), msg
+        assert len(tile_json) == len(directory), msg
 
-    # Convert buffer from tuple of strings to tuple of tuple of int
-    # I.e. -b "0 1 2" -b "2" is passed into this function as ("0 1 2", "2"), to
-    # be converted to
-    # [[0, 1, 2], [2]]
-    new_buffer = []
-    for s in buffer:
-        new_buffer.append([int(num) for num in re.split(r'[ ,]', s)])
+    # Make sure that min_zoom and directory have same dimensions
+    if min_zoom:
+        msg = 'min-zoom and directory must be provided the same number of times'
+        assert len(min_zoom) == len(directory), msg
+
+    # Make sure that min_zoom and directory have same dimensions
+    if max_zoom:
+        msg = 'max-zoom and directory must be provided the same number of times'
+        assert len(max_zoom) == len(directory), msg
+
+    # Convert buffer from str to tuple of float
+    # I.e. -b "0 1 2" is passed into this function as "0 1 2", to be converted
+    # to [0, 1, 2]
+    buffer_dists = list(map(float, re.split(r'[ ,]', buffer)))
 
     _package_tiles(
-        geometry=geometry,
-        buffer=new_buffer,
-        directory=directory,
-        tile_json=tile_json,
-        output=output,
+        geometry_path=geometry,
+        buffer_dists=buffer_dists,
+        src_dirs=directory,
+        tile_jsons=tile_json,
+        min_zooms=min_zoom,
+        max_zooms=max_zoom,
+        out_dir=output,
         raise_errors=raise_errors)
 
 
