26315e8c18401a13fe87dbb67fa271dd0a5757ff
Kyle Barron <barronk@mit.edu>
2019-12-13 14:26:12 -0700

    Pass args to package_tiles submodule

--- code/main.py
+++ code/main.py
@@ -1,5 +1,7 @@
 import click
 
+from .package_tiles import package_tiles as _package_tiles
+
 
 @click.group()
 def main():
@@ -12,6 +14,8 @@ def main():
     '--geometry',
     multiple=True,
     required=True,
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, resolve_path=True),
     help=
     'Geometries to use for packaging tiles. Can be any format readable by GeoPandas.'
 )
@@ -26,7 +30,8 @@ def main():
 @click.option(
     '-d',
     '--directory',
-    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, resolve_path=True),
     required=True,
     multiple=True,
     help=
@@ -35,7 +40,8 @@ def main():
 @click.option(
     '-t',
     '--tile-json',
-    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, resolve_path=True),
     required=False,
     multiple=True,
     help=
@@ -44,7 +50,7 @@ def main():
 @click.option(
     '-o',
     '--output',
-    type=click.Path(exists=False, writable=True),
+    type=click.Path(exists=False, writable=True, resolve_path=True),
     required=True,
     help='Output path')
 @click.option(
@@ -55,13 +61,27 @@ def main():
     'Whether to raise an error if a desired tile is not found in the directory.'
 )
 def package_tiles(geometry, buffer, directory, tile_json, output, raise_errors):
-    click.echo(geometry)
-    click.echo(buffer)
-    click.echo(directory)
-    click.echo(tile_json)
-    click.echo(output)
-    click.echo(raise_errors)
-    click.echo('Initialized the database')
+    """Package tiles into zip based on distance from trail
+
+    Example:
+    python main.py package-tiles -g ../data/pct/polygon/bound/town/ca/acton.geojson -b "0 1 2" -d ~/Desktop -o out.zip
+    """
+    # Make sure that buffer and geometry have same dimensions
+    msg = 'geometry and buffer must be provided the same number of times'
+    assert len(geometry) == len(buffer), msg
+
+    # Make sure that buffer and geometry have same dimensions
+    if tile_json:
+        msg = 'tile-json and directory must be provided the same number of times'
+        assert len(geometry) == len(buffer), msg
+
+    _package_tiles(
+        geometry=geometry,
+        buffer=buffer,
+        directory=directory,
+        tile_json=tile_json,
+        output=output,
+        raise_errors=raise_errors)
 
 
 if __name__ == '__main__':
