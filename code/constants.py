324009dce88b990faff0801a471179526be28837
Kyle Barron <barronk@mit.edu>
2019-12-13 21:06:15 -0700

    Refactor constants into own file

---
+++ code/constants.py
@@ -0,0 +1,41 @@
+VALID_TRAIL_CODES = ['pct']
+VALID_TRAIL_SECTIONS = {
+    'pct': ['ca_south', 'ca_central', 'ca_north', 'or', 'wa']
+}
+TRAIL_HM_XW = {
+    'ca_south': ['ca_a', 'ca_b', 'ca_c', 'ca_d', 'ca_e'],
+    'ca_central': ['ca_f', 'ca_g', 'ca_h', 'ca_i', 'ca_j', 'ca_k'],
+    'ca_north': ['ca_l', 'ca_m', 'ca_n', 'ca_o', 'ca_p', 'ca_q', 'ca_r'],
+    'or': ['or_b', 'or_c', 'or_d', 'or_e', 'or_f', 'or_g'],
+    'wa': ['wa_h', 'wa_i', 'wa_j', 'wa_k', 'wa_l'],
+}
+TRAIL_TOWNS_XW = {
+    'ca_south': [
+        'acton', 'agua_dulce', 'big_bear_lake', 'cabazon', 'cajon_pass',
+        'campo', 'hikertown', 'idyllwild', 'julian', 'lake_morena',
+        'mount_laguna', 'tehachapi', 'warner_springs'
+    ],
+    'ca_central': [
+        'bishop', 'donner_pass', 'independence', 'inyokern',
+        'kennedy_meadows_north', 'kernville', 'lake_isabella', 'lone_pine',
+        'mammoth_lakes', 'markleeville', 'mojave', 'reds_meadow', 'ridgecrest',
+        'south_lake_tahoe', 'truckee', 'tuolumne_meadows', 'vvr',
+        'yosemite_village', 'tehachapi'
+    ],
+    'ca_north': [
+        'belden', 'bucks_lake', 'burney', 'burney_falls', 'castella', 'chester',
+        'drakesbad', 'dunsmuir', 'etna', 'mount_shasta', 'old_station',
+        'seiad_valley', 'sierra_city', 'soda_springs', 'truckee', 'weed',
+        'ashland', 'callahans_lodge'
+    ],
+    'or': [
+        'ashland', 'bend', 'callahans_lodge', 'cascade_locks', 'diamond_lake',
+        'fish_lake', 'government_camp', 'mazama_village', 'olallie_lake',
+        'rim_village', 'sisters', 'timberline'
+    ],
+    'wa': [
+        'cascade_locks', 'leavenworth', 'mazama', 'packwood', 'skykomish',
+        'snoqualmie_pass', 'stehekin', 'stevens_pass', 'stevenson',
+        'trout_lake', 'white_pass', 'winthrop'
+    ],
+}
