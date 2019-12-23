3ea97098d660956b8d0d2ba4ebe524921e35c6bd
Kyle Barron <barronk@mit.edu>
2019-12-22 23:03:56 -0700

    Working geotagging

--- code/data_source/photos.py
+++ code/data_source/photos.py
@@ -1,11 +1,15 @@
 import json
-from datetime import datetime
-from dateutil import parser
 from pathlib import Path
 from subprocess import run
 from typing import List, Union
 
+import osxphotos
+import pandas as pd
+from keplergl_quickvis import Visualize
+from shapely.geometry import LineString
 
+
+# self = PhotosLibrary()
 class PhotosLibrary:
     """Geotag photos from hike
 
@@ -20,30 +24,93 @@ class PhotosLibrary:
     def __init__(self):
         super(PhotosLibrary, self).__init__()
 
-        s = '~/Pictures/Photos Library.photoslibrary/Masters'
-        self.photos_dir = Path(s).expanduser()
+        self.photos_dir = Path('~/Pictures').expanduser()
 
-    def find_photos(self, start_date='2019-04-22', end_date='2019-10-03'):
+    def geotag_photos(photos, points):
+        """Geotag photos
+
+        It appears that my photos are uniformly 1 hour behind the accurate time.
+        For some reason, my photos are listed as having UTC-6, when Pacific
+        Daylight Time is UTC-7.
+
+        Since offsets from UTC are confusing, here's an example: If I took a
+        photo at 10:00AM set to UTC-6, the timestamp would be 16:00Z. In UTC-7,
+        that would be 9:00AM. Since the actual time was 10AM in UTC-7, I need to
+        add one hour so that the UTC time is correct.
+
+        Still a little confusing to me, but my manual checks confirmed that
+        adding 1 hour gave accurate geocoding at both the start and end of the
+        trail.
+
+        Args:
+            - photos: list of osxphotos.PhotoInfo instances
+            - points: DataFrame of points from watch
+        """
+        geotagged_points = []
+        for photo in photos:
+            # Convert photo's date to a pandas Timestamp object in UTC
+            dt = pd.to_datetime(photo.date, utc=True)
+            # Fix timestamp by adding 1 hour
+            dt += pd.DateOffset(hours=1)
+
+            # Find closest point previous in time
+            idx = points.index.get_loc(dt, method='pad')
+            # Get the two nearest rows
+            rows = points.iloc[idx:idx + 2]
+
+            # Linearly interpolate between the two rows' timestamps and the
+            # photo's timestamp
+            # Difference in time b
+            a = rows.index[0].tz_convert(None)
+            b = rows.index[1].tz_convert(None)
+            c = dt.tz_convert(None)
+            # Percentage of the way from a to b
+            pct = (c - a) / (b - a)
+            # Line between the two points
+            line = LineString([rows.iloc[0].geometry, rows.iloc[1].geometry])
+            # Find interpolated point
+            interp = line.interpolate(pct, normalized=True)
+
+            geotagged_points.append(interp)
+
+        Visualize(geotagged_points, style='outdoors')
+
+    def find_photos(self, album='nst-guide-web'):
         """Recursively find photos that were taken between dates
 
         Args:
-            start_date
-            end_date
+            - album: name of album
+
+        Returns:
+            List of osxphotos.PhotoInfo
         """
-        start_date = parser.parse(start_date)
-        end_date = parser.parse(end_date)
+        photosdb = osxphotos.PhotosDB()
+        assert album in photosdb.albums, f'Album {album} not found'
+        photos = photosdb.photos(albums=[album])
+        return photos
 
-        years = range(start_date.year, end_date.year + 1)
+    def get_photos_metadata(self, overwrite=False):
+        """
+        Deprecated: used with exiftool but osxphotos seems to be good enough for
+        my needs
+        """
+        metadata_path = self.photos_dir / 'metadata.json'
+        if overwrite or (not metadata_path.exists()):
+            self._run_exiftool_metadata()
 
-        recurse_dir = self.photos_dir / '2019'
-        # years =
-        self.photos_dir
+        # Load metadata
+        with open(metadata_path) as f:
+            metadata = json.load(f)
 
-        f = '/Users/kyle/IMG_1208.HEIC'
-        paths = ['/Users/kyle/IMG_0162.HEIC']
-        folder = Path('/Users/kyle/tmp/')
-        str(folder)
+        return metadata
 
+    def _run_exiftool_metadata(self):
+        """
+        Deprecated: used with exiftool but osxphotos seems to be good enough for
+        my needs
+        """
+        cmd = 'exiftool -n -j -r Photos\ Library.photoslibrary/originals > metadata.json'
+        run(cmd, check=True, shell=True, cwd=self.photos_dir)
 
     def get_metadata_for_folder(folder: Union[str, Path], ext='.HEIC'):
         """Recursively get metadata for all images within folder
@@ -53,9 +120,7 @@ class PhotosLibrary:
             folder = folder.rstrip('/')
 
         recurse_dir = f'"{str(folder)}/"'
-        cmd = [
-            'exiftool', '-j', recurse_dir
-        ]
+        cmd = ['exiftool', '-j', recurse_dir]
         # -j json output
         # -r recurse directory
         # -n Prevent pretty print formatting. Also gives lat/lon GPS coords
@@ -65,18 +130,7 @@ class PhotosLibrary:
         json.loads(res.stdout)
 
     def get_metadata_for_files(paths: List[Union[str, Path]]):
-        cmd = [
-            'exiftool',
-            '-j',
-            '-n',
-            *[str(x) for x in paths]
-        ]
+        cmd = ['exiftool', '-j', '-n', *[str(x) for x in paths]]
         res = run(cmd, capture_output=True)
         return json.loads(res.stdout)
         res.stderr
-
-
-
-
-
-        # /Users/kyle/Pictures/Photos Library.photoslibrary/Masters/2019/09/24/20190924-160208
