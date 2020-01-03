6f790a346dd647cbab4af4537a227a8cdeab504a
Kyle Barron <barronk@mit.edu>
2020-01-02 22:06:04 -0700

    Compress current fire perimeters GeoJSON with gzip

--- code/lambda/nifc_current.py
+++ code/lambda/nifc_current.py
@@ -10,6 +10,7 @@ Layers used:
 - Klayers-python37-requests: arn:aws:lambda:us-east-1:113088814899:layer:Klayers-python37-requests:9
 """
 
+import gzip
 import json
 
 import boto3
@@ -23,12 +24,19 @@ def lambda_handler(event, context):
     """AWS Lambda entry point"""
     nifc = NIFCCurrent()
     gj = nifc.geojson()
+
+    # Set separators so that there are no useless spaces in the GeoJSON file
     minified = json.dumps(json.loads(str(gj)), separators=(',', ':'))
+
+    # Compress using gzip
+    compressed = gzip.compress(minified.encode('utf-8'))
 
     # Write individual GeoJSON file to S3
     obj = s3.Object('tiles.nst.guide', f'nifc/current.geojson')
     # 2-hour cache plus 24-hour stale-while-revalidate
     obj.put(
-        Body=minified,
+        Body=compressed,
         ContentType='application/geo+json',
+        ACL='public-read',
+        ContentEncoding='gzip',
         CacheControl='public, max-age=7200, stale-while-revalidate=86400')
